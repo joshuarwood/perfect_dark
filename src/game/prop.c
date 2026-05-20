a5ecf2326b013005613731b6560be2d1ef85441d
joshuarwood <joshua.randall.wood@gmail.com>
2026-05-19 23:16:29 -0500

    use expensive version of chr test for lights since I prefer the accuracy during cutscenes. The impact is pretty low on my PC.

--- src/game/prop.c
+++ src/game/prop.c
@@ -1065,7 +1065,7 @@ bool shotTestLos(struct coord *gunpos2d, struct coord *gundir2d, struct coord *g
 		if (prop) {
 			if (prop->type == PROPTYPE_CHR
 					|| (prop->type == PROPTYPE_PLAYER && prop->chr && (g_Vars.in_cutscene || playermgrGetPlayerNumByProp(prop) != g_Vars.currentplayernum))) {
-				chrTestHit(prop, &shotdata, false, true);
+				chrTestHit(prop, &shotdata, false, false);
 			} else if (prop->type == PROPTYPE_WEAPON || (prop->type == PROPTYPE_DOOR && ((struct doorobj *)prop->obj)->doortype != DOORTYPE_LASER)
 					|| (prop->type == PROPTYPE_OBJ && prop->obj->type != OBJTYPE_GLASS && prop->obj->type != OBJTYPE_TINTEDGLASS)) {
 				objTestHit(prop, &shotdata);
